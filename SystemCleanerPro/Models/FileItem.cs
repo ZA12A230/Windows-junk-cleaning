5c23698e98395a516a0598d40462b86788cdbf98
ZA12A230 <ZA12A230@users.noreply.github.com>
2026-05-17 12:57:34 +0000

    feat: 构建 Windows 系统清理工具
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- SystemCleanerPro/Models/FileItem.cs
+++ SystemCleanerPro/Models/FileItem.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace SystemCleanerPro.Models;
 
 public enum FileCategory
--- SystemCleanerPro/Models/ScanConfiguration.cs
+++ SystemCleanerPro/Models/ScanConfiguration.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace SystemCleanerPro.Models;
 
 public class ScanConfiguration
